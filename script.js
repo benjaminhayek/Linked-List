2cc01b316003440e7c89896b02b63ec9d66d735f
bhayek12@gmail.com <Ben@Bens-MacBook-Pro.local>
2018-07-08 15:02:47 -0600

    Add template literal

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 var addBtn = document.querySelector(".add-button");
-var deleteBtn = document.querySelector(".delete-btn");
-var readBtn = document.querySelector(".read-btn");
-var urlOutput = document.querySelector(".url-output");
-var titleOutput = document.querySelector(".title-output");
 var urlInput = document.querySelector(".url-input");
 var titleInput = document.querySelector(".title-input");
+var cardList = document.querySelector('.list')
 
 addBtn.addEventListener('click', function() {
-  titleOutput.innerText += titleInput.value;
-  urlOutput.innerText += urlInput.value;
-  
+   var title = titleInput.value;
+   var url = urlInput.value;
+   console.log()
+var card = `
+	<article class="bookmark">
+      <h2 class="title-output">${title}</h2>
+      <p class="url-output">${url}</p>
+      <div class="buttons">
+        <button class="read-btn">Read</button>
+        <button class="delete-btn">Delete</button>
+      </div>
+    </article>
+`
+	cardList.innerHTML += card
 })
+
+
+
+// function addBookmark()
+
+
+
+
+	
